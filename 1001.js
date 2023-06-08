0039b73d146ed878e01f994ce96e936ee12a05e5
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-08 20:38:40 +0600

    Solved 1001

---
+++ 1001.js
@@ -0,0 +1,8 @@
+// Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+let A = parseInt(10);
+let B = parseInt(9);
+
+let X = A + B;
+
+console.log(X);
