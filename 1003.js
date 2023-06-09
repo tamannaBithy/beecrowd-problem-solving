a2d9d795f76c185a19a6673e242040109fdd7088
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-09 17:51:47 +0600

    Solved 1003

---
+++ 1003.js
@@ -0,0 +1,16 @@
+// Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
+
+// Input
+// The input file contains 2 integer numbers.
+
+// Output
+// Print the message "SOMA" with all the capital letters, with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B.
+
+
+
+const A = parseInt(20);
+const B = parseInt(-30);
+
+const SOMA = A + B
+
+console.log(SOMA);
