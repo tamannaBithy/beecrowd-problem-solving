6b652603e9323e898b439ff6e62c92424e9cdb13
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-09 17:38:33 +0600

    Solved 1002

---
+++ 1002.js
@@ -0,0 +1,16 @@
+// The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+
+
+// Input
+// The input contains a value of floating point (double precision), that is the variable R.
+
+// Output
+// Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error"
+
+
+const π = 3.14159;
+let R = 2.2
+
+const A = π * Math.pow(R, 2)
+ 
+console.log("A = ", A.toFixed(4));
