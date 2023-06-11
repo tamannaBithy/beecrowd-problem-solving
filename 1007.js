e374883494220f2888302abfefe3f48eeb0bb3e4
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-11 12:52:20 +0600

    Solved 1007

---
+++ 1007.js
@@ -0,0 +1,14 @@
+// Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+// Input
+// The input file contains 4 integer values.
+
+const a = parseInt(5);
+const b = parseInt(6);
+const c = parseInt(7);
+const d = parseInt(8);
+
+
+const difference = a * b - c * d
+
+console.log(difference);
