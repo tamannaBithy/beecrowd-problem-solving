05d7c9cbe30ba05bbcc159f493fd06656182185d
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-14 19:48:41 +0600

    Solved 1013

---
+++ 1013.js
@@ -0,0 +1,16 @@
+// Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+
+
+
+// Input
+// The input file contains 3 integer values.
+
+
+const a = parseInt(7)
+const b = parseInt(14)
+const s = parseInt(106)
+
+
+const result = Math.max(a, b, s)
+
+console.log(result);
