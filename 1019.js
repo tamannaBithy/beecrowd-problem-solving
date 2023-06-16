e37131047408dc19bac7937aa7d164743b5d750c
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-16 22:09:35 +0600

    Solved 1019

--- 1019.js
+++ 1019.js
@@ -17,3 +17,18 @@ for (const time of timer) {
 }
 
 console.log(`${timeFormat[0]} : ${timeFormat[1]} : ${value}`);
+
+
+
+//// 2nd way ///
+
+
+const durationInSeconds = 556
+
+// Calculate the hours, minutes, and remaining seconds
+const hours = Math.floor(durationInSeconds / 3600);
+const minutes = Math.floor((durationInSeconds % 3600) / 60);
+const seconds = durationInSeconds % 60;
+
+// Output the duration in the format HH:MM:SS
+console.log(`${hours}:${minutes}:${seconds}`);
