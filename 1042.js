0f798d3ab19a5693a2d8b4db506a48fb13780c08
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-20 17:21:05 +0600

    Solved 1042

---
+++ 1042.js
@@ -0,0 +1,11 @@
+// Read three integers and sort them in ascending order. After, print these values in ascending order, a blank line and then the values in the sequence as they were readed.
+
+// Input
+// The input contains three integer numbers.
+
+
+const numArr = [7, 21, -14];
+
+const sortedNum = numArr.sort((a, b) => a - b)
+
+console.log(sortedNum);
