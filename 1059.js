06b80dc5a7b69729e6cf083358f1eef48e5dfe46
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-06-23 19:56:33 +0600

    Solved 1059

---
+++ 1059.js
@@ -0,0 +1,11 @@
+// Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+
+// Input
+// In this extremely simple problem there is no input.
+
+
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
