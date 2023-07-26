7face4e21397c59fbf833c61daa70f36ab6e6ca1
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-07-26 17:19:24 +0600

    Solved 1179

--- 1179.js
+++ 1179.js
@@ -1,7 +1,5 @@
 // In this problem you need to read 15 numbers and must put them into two different arrays: par if the number is even or impar if this number is odd. But  the size of each of the two arrrays is only 5 positions. So every time you fill one of two arrays, you must print the entire array to be able to use it again for the next numbers that are read. At the end, all remaining numbers of each one of these two arrays must be printed beggining with the odd array. Each array can be filled how many times are necessary.
 
-
-
 const array = [1, 3, 4, -4, 2, 3, 8, 2, 5, -7, 54, 76, 789, 23, 98];
 const evenNumbers = [];
 const oddNumbers = [];
